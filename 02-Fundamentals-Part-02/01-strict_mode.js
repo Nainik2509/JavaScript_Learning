035f9dbfc8ee326af1f2ee1f17cb15e73337c8d2
Nainik <nainikmehta1999@gmail.com>
2022-01-23 23:52:06 +0530

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-02/01-strict_mode.js
@@ -0,0 +1,31 @@
+/**
+ * This file is part of [JAVASCRIPT_LEARNING]
+ *
+ * (c) 2022 [Nainik Mehta] 
+ * <[https://www.linkedin.com/in/nainik-mehta-25nk12]>
+ * <[https://github.com/Nainik2509/JavaScript_Learning]>
+ *
+ * --------------------------------------------------
+ *
+ * @module JavaScript - Activating Strict Mode
+ * @author [Nainik Mehta] <[nainikmehta1999@gmail.com]>
+ * @version 1.0.0
+ *
+ * 
+ * -> 
+ * 
+ * --------------------------------------------------
+ */
+
+////////////////////////////////////////////////////////////////
+
+'use strict';
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
