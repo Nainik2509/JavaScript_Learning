62d233817a3de45838adfc9685581657130f7737
Nainik <nainikmehta1999@gmail.com>
2022-01-24 00:08:07 +0530

    Function calling other functions

--- 02-Fundamentals-Part-02/01-functions.js
+++ 02-Fundamentals-Part-02/01-functions.js
@@ -94,25 +94,3 @@ function fruitProcessor(apples, oranges) {
 }
 console.log(fruitProcessor(2, 3));
 
-
-////////////////////////////////////////////////////////////////
-// Reviewing Functions
-const calcAge = function (birthYeah) {
-    return 2037 - birthYeah;
-}
-
-const yearsUntilRetirement = function (birthYeah, firstName) {
-    const age = calcAge(birthYeah);
-    const retirement = 65 - age;
-
-    if (retirement > 0) {
-        console.log(`${firstName} retires in ${retirement} years`);
-        return retirement;
-    } else {
-        console.log(`${firstName} has already retired 🎉`);
-        return -1;
-    }
-}
-
-console.log(yearsUntilRetirement(1991, 'Jonas'));
-console.log(yearsUntilRetirement(1950, 'Mike'));
