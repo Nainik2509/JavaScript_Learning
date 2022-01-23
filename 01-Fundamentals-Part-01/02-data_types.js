298cce8f0a279fcb025d845ff9d67863005b384d
Nainik <nainikmehta1999@gmail.com>
2022-01-23 22:57:56 +0530

    Truthy and Falsy values

--- 01-Fundamentals-Part-01/02-data_types.js
+++ 01-Fundamentals-Part-01/02-data_types.js
@@ -29,6 +29,7 @@
  * -> var                   - Should be avoided. 
  * 
  * -> Type Conversion and Coercion
+ * -> Truthy & Falsy values
  * 
  * --------------------------------------------------
  */
@@ -93,3 +94,28 @@ console.log('23' / '2');
 let n = '1' + 1; // '11'
 n = n - 1;
 console.log(n);
+
+
+////////////////////////////////////////////////////////////////
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 100;
+if (money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED');
+}
