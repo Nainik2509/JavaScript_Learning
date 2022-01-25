e1f974c185078810e486674d078dd4487744b85c
Nainik <nainikmehta1999@gmail.com>
2022-01-26 00:12:00 +0530

    Objects in JavaScript

---
+++ 02-Fundamentals-Part-02/04-objects.js
@@ -0,0 +1,36 @@
+/**
+ * This file is part of [JAVASCRIPT_LEARNING]
+ *
+ * (c) 2022 [Nainik Mehta]
+ * <[https://www.linkedin.com/in/nainik-mehta-25nk12]>
+ * <[https://github.com/Nainik2509/JavaScript_Learning]>
+ *
+ * --------------------------------------------------
+ *
+ * @module JavaScript - Objects
+ * @author [Nainik Mehta] <[nainikmehta1999@gmail.com]>
+ * @version 1.0.0
+ *
+ *
+ * -> Objects :
+ *
+ * --------------------------------------------------
+ */
+
+////////////////////////////////////////////////////////////////
+// Introduction to Objects
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
